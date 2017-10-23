fe2755153d3442acaa2aef6de1db6b6cd25d18e4
diana4545 <diana4545@outlook.com>
2017-10-23 06:17:47 +0200

    change to my app

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ ini_set('always_populate_raw_post_data', '-1');
 error_reporting(E_WARNING);
 session_start();
 
-$con = mysqli_connect ("localhost", "thirsub3_epl", "(kGl~aF92H0#", 'thirsub3_dookie');
+$con = mysqli_connect ("localhost", "thirsub3_epl", "thirsub3_epl", 'thirsub3_epl');
 
 if (mysqli_connect_errno())
   {
@@ -11,12 +11,12 @@ if (mysqli_connect_errno())
   
   }
 
-define("SITE_URL", "https://www.thirstysoftware.com/externalproductlinks");
-define("SHOPIFY_API_KEY", "f4f251d863225596a53183a211c345dd");
-define("SHOPIFY_SECRET", "3efda1bc0ea2bae5f0f47a8bf8fee0bd");
+define("SITE_URL", "https://kanvaskreations.com/epl");
+define("SHOPIFY_API_KEY", "447fd7b272a0637f11bc556aac8ea625");
+define("SHOPIFY_SECRET", "6497c2dc868839fb9e0a103729911de6");
 define("SHOPIFY_SCOPE", "read_products,write_products,read_script_tags,write_script_tags,read_themes,write_themes");
 define("REDIRECT_URL", SITE_URL."/auth.php");
-define("SHOPIFY_TOKEN", "e0f1f5e52beb8f90b25902fd7dceb9e3-1490578021");
+define("SHOPIFY_TOKEN", "f5dcd183db64309f3e9c8c9377374268-1508596168");
 define("TRIAL_DAYS", 2);
 define("APP_COST",4.99);
 
